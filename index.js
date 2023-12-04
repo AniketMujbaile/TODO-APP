0a6b577f4f84fcf627ee3c88c80167c7030294f5
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2023-12-04 08:39:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 // set up the port number
-const port = 7000;
+const port = process.env.PORT || 7000;
 
 // importing the DataBase
 const db = require('./config/mongoose');
